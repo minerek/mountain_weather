0431c1abb85fc5f54f9f092293da334d77bfe460
Michal Gacek <michal.gacek@pl.ibm.com>
2026-09-15 14:21:47 +0200

    fix: update Cubryna lat to 49.1878

--- app.py
+++ app.py
@@ -462,7 +462,7 @@ SZCZYTY = {
     "Mięguszowiecki Szczyt Wielki":                  (49.1798, 20.0617, 2438, "Tatry Polskie"),
     "Mięguszowiecki Szczyt Czarny":                  (49.1893, 20.0653, 2410, "Tatry Polskie"),
     "Mięguszowiecki Szczyt Pośredni":                (49.1882, 20.0619, 2393, "Tatry Polskie"),
-    "Cubryna":                                       (49.1895, 20.0536, 2376, "Tatry Polskie"),
+    "Cubryna":                                       (49.1878, 20.0536, 2376, "Tatry Polskie"),
     "Szpiglasowy Wierch (Hrubý štít)":               (49.2015, 20.0417, 2172, "Tatry Polskie"),
     "Kościelec":                                     (49.2247, 20.0152, 2155, "Tatry Polskie"),
     "Zawrat":                                        (49.2195, 20.0161, 2159, "Tatry Polskie"),
